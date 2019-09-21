2fb0535b4f6b6cd55f8ead62ef0589fb448955e5
margie <ly.margarett@gmail.com>
2019-09-20 18:53:37 -0700

    add initial API calls for flight info

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+//npm module: creates a new instance of the unirest client
+const unirest = require("unirest");
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +14,26 @@ app.use('/assets', express.static(path.join(__dirname, '/../client/assets')))
 app.get('/', (req ,res) => {
   res.status(200).sendFile(path.join(__dirname + '/../index.html'));
 });
+
+
+
+//get request for a certain query: notice the route after /browsequotes/. We specify the country the user is in /US/, the currency /USD/, the locale you want the results in /en-US/, and airport from/SFO-sky/, to /LAX-sky, and outbound partial date /anytime.
+var req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/LAX-sky/anytime");
+
+//headers for the request to skyscanner, the host and unique KEY for access
+req.headers({
+    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    "x-rapidapi-key": "2b3aee7f7emsh9a3043bf4a78f25p186a4ejsn07fdb9fc52ed"
+});
+
+//on a response expect a response of queried data
+req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+    console.log(res.body);
+});
+
+
+
 
 app.all('*', (req, res) => {
   res.sendStatus(404);
